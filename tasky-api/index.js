e460532a9e5fe4efd98df92d4a253f882591ba90
deirdreOH <dohalloran@wit.ie>
2025-11-21 15:40:35 +0000

    added Users Router

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -1,13 +1,18 @@
 import './db';
+// other imports
+import cors from 'cors';
 
 import dotenv from 'dotenv';
 import express from 'express';
 import tasksRouter from './api/tasks';
+//... other imports
+import usersRouter from './api/users';
+
 
 
 dotenv.config();
-
-const errHandler = (err, req, res, next) => {
+const errHandler = (err, req, res) => {
+    //  const errHandler = (err, req, res, next) => {
     /* if the error in development then send stack trace to display whole error,
     if it's in production then just send error message  */
     if (process.env.NODE_ENV === 'production') {
@@ -17,6 +22,9 @@ const errHandler = (err, req, res, next) => {
 };
 
 const app = express();
+// Enable CORS for all requests
+app.use(cors());
+
 
 const port = process.env.PORT;
 
@@ -24,6 +32,10 @@ app.use(express.json());
 
 
 app.use('/api/tasks', tasksRouter);
+
+//Users router
+app.use('/api/users', usersRouter);
+
 
 app.use(errHandler);
 
