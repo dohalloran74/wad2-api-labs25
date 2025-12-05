b0ce635b5e29731c648d59e9449df939ac6c4edd
deirdreOH <dohalloran@wit.ie>
2025-12-05 15:43:58 +0000

    Authentication front end set up

---
+++ tasky-react/src/components/Form.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+const AddTaskForm = (props) => {
+  return (
+    <div>
+      <form onSubmit={props.submit}>
+        <label>
+          Task title:
+          <input
+            type="text"
+            name="title"
+            required
+            onChange={(event) => props.change(event)}
+          />
+        </label>
+        <br />
+        <label>
+          Due date:
+          <input
+            type="date"
+            name="deadline"
+            required
+            onChange={(event) => props.change(event)}
+          />
+        </label>
+        <br />
+        <label>
+          Details:
+          <input
+            type="text"
+            name="description"
+            onChange={(event) => props.change(event)}
+          />
+        </label>
+
+        {/*Add in new priortiy field */}
+
+        <label>
+          Priority:
+          <select name="priority" onChange={(event) => props.change(event)}>
+            <option  value="Low">
+              Low
+            </option>
+            <option key="Medium" value="Medium">
+              Medium
+            </option>
+            <option key="High" value="High">
+              High
+            </option>
+          </select>
+        </label>
+
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
+
+export default AddTaskForm;
